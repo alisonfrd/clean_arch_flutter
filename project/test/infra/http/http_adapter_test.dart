15f2f9edb678daff8bd03e674c81cf81cc9c6436
alisonfrd <alisonfrd@outlook.com>
2021-11-16 08:17:37 -0400

    garantido que sera lancado NotFoundError  no erro 404

--- project/test/infra/http/http_adapter_test.dart
+++ project/test/infra/http/http_adapter_test.dart
@@ -102,6 +102,13 @@ void main() {
       expect(future, throwsA(HttpError.forbidden));
     });
 
+    test('deve retornar NotFoundError se o statusCode for 404', () async {
+      mockResponse(404);
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test('deve retornar ServerError se o statusCode for 500', () async {
       mockResponse(500);
       final future = sut.request(url: url, method: 'post');
